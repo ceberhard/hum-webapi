0578700c5e1b9048e4961e4594ab621b8d54a4ab
Christopher Eberhard <eberhac@epi.usf.edu>
2019-01-06 18:55:45 -0500

    Added some basic constraints on the initial tables ddl

--- config/ddl.sql
+++ config/ddl.sql
@@ -2,13 +2,15 @@ CREATE TABLE task_item
 (
     id INTEGER PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description VARCHAR(1000) NULL
+    description VARCHAR(1000) NULL,
+    CONSTRAINT task_item_c1 UNIQUE(title)
 );
 
 CREATE TABLE task_status
 (
     id INTEGER PRIMARY KEY,
-    status VARCHAR(50) NOT NULL
+    status VARCHAR(50) NOT NULL,
+    CONSTRAINT task_status_c1 UNIQUE(status)
 );
 
 CREATE TABLE task_history
@@ -18,7 +20,8 @@ CREATE TABLE task_history
     task_status_id INTEGER NOT NULL,
     status_dttm VARCHAR(30) NOT NULL,
     FOREIGN KEY(task_id) REFERENCES task_item(id),
-    FOREIGN KEY(task_status_id) REFERENCES task_status(id)
+    FOREIGN KEY(task_status_id) REFERENCES task_status(id),
+    CONSTRAINT task_history_c1 UNIQUE(task_id, task_status_id, status_dttm)
 );
 
 
